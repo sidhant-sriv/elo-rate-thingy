7f9e719df9e23133484a1950f354f9e6a257cd9e
Aditya Chowdhary <adityachowdhary04@gmail.com>
2023-05-18 20:02:54 +0530

    Created functions, docstrings

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
-class Teacher(Base):
+class Player(Base):
     __tablename__ = 'Teachers'
 
     id = Column("id", Integer, primary_key=True, autoincrement=True)
@@ -20,6 +20,63 @@ class Teacher(Base):
     def __repr__(self):
         return f"{self.id}: {self.name}({self.rating}) - {self.description}"
     
+def displayAll() -> list[Player]:
+    '''Displays all players
+        returns:
+            List of Player object
+    '''
+    results = session.query(Player).all()
+    return results
+
+def displayRating(rating: int) -> list[Player]:
+    '''Displays player by rating
+        parameters:
+            id[int]: ID of the player
+        returns:
+            List of Player object
+    '''
+    results = session.query(Player).filter(Player.rating>rating).all()
+    return results
+
+def displayID(id: int) -> Player:
+    '''Displays player by ID
+        parameters:
+            id[int]: ID of the player
+        returns:
+            Player object
+    '''
+    results = session.query(Player).filter(Player.id == id).first()
+    return results
+
+def addPlayer(name: String, rating: int, description: String | None = None):
+    '''Adds a new player to the database
+        Parameters:
+            name[String]: Name of the player
+            rating[int]: Rating of the player
+            description[String]: Description of the player
+    '''
+    player = Player(name=name, rating=rating, description=description)
+    session.add(player)
+    session.commit()
+
+def updatePlayer(id: int, rating_new: int):
+    '''Updates rating of a player
+        Parameters:
+            id[int]: ID of the player to change
+            rating_new[int]: The updated rating of the player
+    '''
+    result = session.query(Player).filter(Player.id == id).first()
+    result.rating = rating_new
+    session.commit()
+
+def deletePlayer(id: int):
+    '''Deletes a player from the database
+        Parameters: 
+            id[int]: ID of player to delete
+    '''
+    results = session.query(Player).filter(Player.id == n).first()
+    session.delete(results)
+    session.commit()
 
 engine = create_engine("sqlite:///mydb.db") #, echo=True)
 Base.metadata.create_all(bind=engine)
@@ -27,44 +84,32 @@ Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-t1 = Teacher(name="Sidhant", rating=1600, description="Googooooogaagaa")
-t2 = Teacher(name="Tony Stark", rating=1500, description="Iron Man")
-t3 = Teacher(name="Dude", rating=1300, description="Wtf am I doing")
-session.add_all([t1, t2, t3])
-session.commit()
+'''
+ALL FOR TESTING PURPOSES
 
-t4 = Teacher(name=input("Name: "), rating=input("Rating: "), description=input("Description: "))
-session.add(t4)
-session.commit()
+addPlayer(name="Sidhant", rating=1600, description="Googooooogaagaa")
+addPlayer(name="Tony Stark", rating=1500, description="Iron Man")
+addPlayer(name="Dude", rating=1300, description="Wtf am I doing")
+addPlayer(name=input("Name: "), rating=input("Rating: "), description=input("Description: "))
 
-print("------ALL'-------")
-results = session.query(Teacher).all()
-for r in results:
+for r in displayAll():
     print(r)
 
-
-print("-------Rating>1400-----")
-results = session.query(Teacher).filter(Teacher.rating>1400).all()
-for r in results:
+for r in displayRating(1400):
     print(r)
 
 print("-------Update---------")
 n = input("Enter id to be changed: ")
 r = int(input("Enter new rating: "))
-results = session.query(Teacher).filter(Teacher.id == n).first()
-results.rating = r
-session.commit()
+updatePlayer(n, r)
 
-print("--------ALL--------")
-for r in session.query(Teacher).all():
+for r in displayAll():
     print(r)
 
 print("--------Delete------")
 n = input("Enter id to delete: ")
-results = session.query(Teacher).filter(Teacher.id == n).first()
-session.delete(results)
-session.commit()
+deletePlayer(n)
 
-print("---------ALL-----------")
-for r in session.query(Teacher).all():
+for r in displayAll():
     print(r)
+'''
